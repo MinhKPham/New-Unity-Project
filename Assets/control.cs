a01446b042ab882359f39aed7078e79db99fceca
MinhKPham <phamm@rpi.edu>
2017-05-24 21:00:15 -0400

    add files

---
+++ Assets/control.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class control : MonoBehaviour {
+    public Transform mainone;
+    int i = 2;
+    public Button button;
+    public Button save;
+    public InputField r;
+    public InputField g;
+    public InputField b;
+    List<Transform> shapes= new List<Transform>() ;
+    float rr=0;
+    float gg=0;
+    float bb=0;
+    // Use this for initialization
+    void Start () {
+        button.onClick.AddListener(shape);
+        save.onClick.AddListener(saveshape);
+        r.text = "0";
+        g.text = "0";
+        b.text = "0";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (r.text == "") { rr = 0; }
+        else rr=float.Parse(r.text);
+        if (g.text == "") { gg = 0; }
+        else gg = float.Parse(g.text);
+        if (b.text == "") { bb = 0; }
+        else bb = float.Parse(b.text);
+        mainone.GetComponent<meshobject>().c= new Color(rr,gg,bb);
+        
+        
+
+    }
+    void shape()
+    {
+        mainone.GetComponent<meshobject>().changeshape();
+    }
+    void saveshape()
+    {
+        Transform newone = Instantiate(mainone, new Vector3(-11f + i * 1.25f, -3, 0), Quaternion.identity);
+        newone.GetComponent<meshobject>().setind(mainone.GetComponent<meshobject>().getind());
+        newone.GetComponent<meshobject>().c = mainone.GetComponent<meshobject>().c;
+        shapes.Add(newone);
+        i++;
+    }
+}
---
+++ Assets/meshobject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshobject : MonoBehaviour {
+    public Mesh[] m;
+    public Color c;
+    int ind = 0;
+    private bool mainone = true;
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<MeshFilter>().sharedMesh = m[ind];
+        GetComponent<MeshRenderer>().material.color=c;
+        
+
+
+    }
+    public void changeshape()
+    {
+        ind++;
+        if (ind > 2) ind = 0;
+    }
+    
+    public int getind()
+    {
+        return ind;
+    }
+    
+    public void setind(int i)
+    {
+        ind=i;
+    }
+}
